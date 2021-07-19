b7a5c9ede2320ae08d3317101c6e5b0ad37e4335
ootakekou <kamanattou@gmail.com>
2021-07-19 15:37:00 +0900

    roomsテーブルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   devise_for :users
-=======
   get 'messages/index'
->>>>>>> parent of ae3ef29 (usersテーブルの作成)
   root to: "messages#index"
   resources :users, only: [:edit, :update]
 end
