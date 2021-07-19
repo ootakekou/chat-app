e439cc01a0a20cf0919a383634d84ec8ba1ee3a9
ootakekou <kamanattou@gmail.com>
2021-07-19 14:52:42 +0900

    バリデーション

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   devise_for :users
+=======
+  get 'messages/index'
+>>>>>>> parent of ae3ef29 (usersテーブルの作成)
   root to: "messages#index"
   resources :users, only: [:edit, :update]
 end
